e5ff418000d10f3ef4cd5f0bbb6f15f1f57dcad2
Volodymyr Haluha <114477418+VolodymyrHaluha@users.noreply.github.com>
2026-06-30 23:25:57 +0300

    Зміни для сайту

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    return send_from_directory(".", "index.html")
+def home():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="192.168.0.1", port=5000, debug=True)
+    app.run(debug=True)
