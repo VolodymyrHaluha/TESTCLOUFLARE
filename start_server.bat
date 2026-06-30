9ab936b674172dc18741f87fcabe49d27aa100c8
Volodymyr Haluha <114477418+VolodymyrHaluha@users.noreply.github.com>
2026-06-30 23:22:28 +0300

    Prepare Flask app for deployment

--- start_server.bat
+++ start_server.bat
@@ -1,9 +1,7 @@
 @echo off
 cd /d "%~dp0"
 
-echo Starting Flask server on all network interfaces.
-echo Open this site at http://192.168.0.1:5000 if this computer uses that IP address.
-echo You can also try http://127.0.0.1:5000 on this computer.
-python -m flask --app app run --host=0.0.0.0 --port=5000
+echo Starting local Flask server at http://127.0.0.1:5000
+python app.py
 
 pause
