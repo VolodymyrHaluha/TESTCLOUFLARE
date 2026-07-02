43c6f2b77af02145a1765b9fbe24085e8835b590
Volodymyr Haluha <114477418+VolodymyrHaluha@users.noreply.github.com>
2026-07-02 09:42:52 +0300

    Навчання по домену

--- start_server.bat
+++ start_server.bat
@@ -2,6 +2,13 @@
 cd /d "%~dp0"
 
 echo Starting local Flask server at http://127.0.0.1:5000
-python app.py
+
+start /B python app.py
+
+timeout /t 5 > nul
+
+echo Starting Cloudflare Tunnel...
+
+"C:\Program Files (x86)\cloudflared\cloudflared.exe" tunnel run --token eyJhIjoiNzM2YmFjNGI0M2I3MTE3ZTAxMTU0OGFmYWI4MGZhZTciLCJ0IjoiMGYyYTE2YjgtNjZlMy00OTI0LTliZjgtOTAyY2FiMjQxZjE3IiwicyI6Ill6ZzFObU0zTlRJdE5UWmpZUzAwTW1Fd0xXRTBZell0WXpneE5ETm1Oakl6T0dGbCJ9
 
 pause
